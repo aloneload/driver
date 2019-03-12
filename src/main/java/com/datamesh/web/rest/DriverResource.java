856ac2ae1a006005550fbbbbde8908e7f3deb9ff
vagrant <vagrant@vagrant.vm>
2019-03-12 03:52:43 +0000

    first comit

---
+++ src/main/java/com/datamesh/web/rest/DriverResource.java
@@ -0,0 +1,33 @@
+package com.datamesh.web.rest;
+
+import com.datamesh.web.rest.vm.LoggerVM;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.LoggerContext;
+import com.codahale.metrics.annotation.Timed;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * Controller for view and managing Log Level at runtime.
+ */
+@RestController
+public class DriverResource {
+
+    @GetMapping("/api/healthz")
+    public String getList() {
+        return "ok";
+    }
+
+    // @PutMapping("/logs")
+    // @ResponseStatus(HttpStatus.NO_CONTENT)
+    // @Timed
+    // public void changeLevel(@RequestBody LoggerVM jsonLogger) {
+    //     LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+    //     context.getLogger(jsonLogger.getName()).setLevel(Level.valueOf(jsonLogger.getLevel()));
+    // }
+}
